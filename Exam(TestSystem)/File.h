8a82a8cb431b1ccc06ea66ea18b27b40ed41225b
Danik Danik <danilprov2004@gmail.com>
2021-10-02 01:03:40 +0300

    02.10.2021

--- Exam(TestSystem)/File.h
+++ Exam(TestSystem)/File.h
@@ -6,7 +6,17 @@
 #include<fstream>
 
 void file() {
+	
 	string path = "tests.txt";
 	fstream data;
-	data.open(path, ofstream::app);
+	data.open(path, fstream::in | fstream::out | fstream::app);
+
+	if (!data.is_open())
+	{
+		cout << "Ошибка открытия файла!" << endl;
+	}
+	else
+	{
+		cout << "Файл открыт!" << endl;
+	}
 }
